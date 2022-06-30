94a8c0d870f4b83db3e67724af4713d8b8caa949
zephyrus21 <pandey.piyus@gmail.com>
2022-07-01 00:50:46 +0530

    revert back to last commit

--- server/src/index.js
+++ server/src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-const { user } = require("./src/routes/user");
+const { user } = require("./routes/user");
 
 const app = express();
 
