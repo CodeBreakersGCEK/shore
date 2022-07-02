86bdfa12b7b33d147ca0791290d0a720be6b2f6c
zephyrus21 <pandey.piyus@gmail.com>
2022-07-02 19:39:43 +0530

    moved to new branch

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,5 @@
-import { createRef, useEffect, useRef, useState } from "react";
-import Timeline from "./Components/Timeline/Timeline";
-import Loader from "./Helper/Components/Loader";
-
 const App = () => {
-  const [isLoader, setIsLoader] = useState(true);
-  const body = createRef();
-
-  useEffect(() => {
-    body.current.style.display = "none";
-    setTimeout(() => {
-      setIsLoader(false);
-    }, 3000);
-    if (isLoader == false) {
-      body.current.style.display = "block";
-    }
-  });
-
-  return (
-    <div className="bg-[#E5E5E5] min-h-screen">
-      <div>
-        {isLoader ? <Loader /> : ""}
-        <div ref={body}>
-          {/* <Profile /> */}
-          <Timeline />
-        </div>
-      </div>
-    </div>
-  );
+  return <div>In development</div>;
 };
 
 export default App;
