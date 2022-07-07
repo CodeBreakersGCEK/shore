5c61bc2994842113f14297079407ecee04041595
zephyrus21 <pandey.piyus@gmail.com>
2022-07-07 13:08:05 +0530

    changed /user/me to /profile

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -13,6 +13,6 @@ const router = express.Router();
 router.route("/register").post(register);
 router.route("/login").post(login);
 router.route("/logout").post(logout);
-router.route("/user/me").get(isLoggedIn, getLoggedInUser);
+router.route("/profile").get(isLoggedIn, getLoggedInUser);
 
 module.exports.user = router;
