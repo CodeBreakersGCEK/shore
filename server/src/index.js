0e74541fc0b2d5e7f6c8f4b9bf55868864666276
zephyrus21 <pandey.piyus@gmail.com>
2022-07-07 21:46:48 +0530

    clean 'main branch'

--- server/src/index.js
+++ server/src/index.js
@@ -1,46 +1,8 @@
-require("dotenv").config();
 const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
-const session = require("express-session");
-
-const { user } = require("./routes/user");
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(cookieParser());
-app.use(cors({ credentials: true }));
-app.use(
-  session({
-    resave: false,
-    secret: "secret",
-    saveUninitialized: false,
-    cookie: {
-      maxAge: 1000 * 60 * 60,
-      httpOnly: true,
-      secure: false,
-      sameSite: "none",
-    },
-  })
-);
-
-app.use(user);
-
 const PORT = process.env.PORT || 4000;
-
-mongoose
-  .connect(process.env.DB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(console.log("Connected to database"))
-  .catch((err) => {
-    console.log(err);
-    process.exit(1);
-  });
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello World" });
