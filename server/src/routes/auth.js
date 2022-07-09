aa022b0a845df3c25daee3ef03f1173c04eecd37
Rakesh Gain <rakeshgain47681@gmail.com>
2022-07-09 20:40:58 +0530

    Login route done

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -38,5 +38,33 @@ router.post('/register', async(req, res) => {
     }
 })
 
+router.post('/login', async (req, res) => {
+    // VALIDATE A DATA BEFORE LOGIN
+    const { error } = loginValidation(req.body);
+    if (error){
+        return res.status(400).send(error.details[0].message);
+    } 
+    // CHECK IF USER EXISTS
+    const user = await User.findOne(
+        {email: req.body.email}
+    );
+    if ( !user ){
+        return  res.status(400).send("User with Email doesnot Exists.!!")
+    } 
+    // CHECK IF PASSWORD IS CORRECT
+    const validPass = await bcrypt.compare(req.body.password, user.password);
+    if (!validPass){
+        return  res.status(400).send("Invalid Password.!!")
+    }
+
+    // Create and assign a token
+    const token = jwt.sign({_id: user._id}, process.env.JWT_SECRET);
+    res.header('auth-token', token);
+    res.json({
+        token: token,
+        user: user
+    });
+})
+
 
 module.exports = router;
