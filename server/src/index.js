94a99204df1a835be5c4a1cfc837ffb024542fb0
Rakesh Gain <rakeshgain47681@gmail.com>
2022-07-09 20:40:15 +0530

    middlewares and auth routs added

--- server/src/index.js
+++ server/src/index.js
@@ -1,32 +1,21 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
-<<<<<<< HEAD
 const cookieParser = require("cookie-parser");
-
-const { user } = require("./routes/user");
-=======
->>>>>>> f6168be (added middlewares)
 const connectDB = require("./config/database_config");
 
 // Import Routes
 const authRoute = require('./routes/auth');
 
 const app = express();
-
-<<<<<<< HEAD
-app.use(bodyParser.json());
-app.use(cookieParser());
-app.use(cors({ credentials: true }));
-=======
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+// app.use(cors({ credentials: true }));
 
 //Route Middlewares
 app.use('/api/user',authRoute)
->>>>>>> f6168be (added middlewares)
 
 
 const PORT = process.env.PORT || 4000;
