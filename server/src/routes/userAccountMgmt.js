57197d27e1a0c850e6c19c1a659806d232e31da3
Rakesh Gain <rakeshgain47681@gmail.com>
2022-07-10 14:35:06 +0530

    auth put patch added

--- server/src/routes/userAccountMgmt.js
+++ server/src/routes/userAccountMgmt.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
+const verify = require('../utils/User/verifyToken');
 
-router.put('/:id', async (req, res) => {
+router.put('/:id',verify, async (req, res) => {
     const user = await User.findByIdAndUpdate(req.params.id, req.body);
     if (!user) return res.status(404).send('The user with the given ID was not found.');
     res.send(user);
 });
 
-router.patch('/:id', async (req, res) => {
+router.patch('/:id',verify, async (req, res) => {
     const user = await User.findByIdAndUpdate(req.params.id, req.body);
     if (!user) return res.status(404).send('The user with the given ID was not found.');
     res.send(user);
