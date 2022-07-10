3f4d01c333eb5f7aea2e950989036ebeabc13b15
Rakesh Gain <rakeshgain47681@gmail.com>
2022-07-10 15:22:46 +0530

    account update with varification done

--- server/src/routes/userAccountMgmt.js
+++ server/src/routes/userAccountMgmt.js
@@ -1,18 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 const verify = require('../utils/User/verifyToken');
 
+// User can Update their complete profile
 router.put('/:id',verify, async (req, res) => {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body);
-    if (!user) return res.status(404).send('The user with the given ID was not found.');
-    res.send(user);
+    try{
+        if(req.body.password){
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(req.body.password, salt);
+            req.body.password = hashedPassword;
+        }
+        const user = await User.findByIdAndUpdate(req.params.id, req.body);
+        if (!user) return res.status(404).send('The user with the given ID was not found.');
+        res.send(user);
+    }catch(err){
+        res.status(400).send(err);
+    }
 });
-
+// User can update a particular field of their account
 router.patch('/:id',verify, async (req, res) => {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body);
-    if (!user) return res.status(404).send('The user with the given ID was not found.');
-    res.send(user);
+    try{
+        if(req.body.password){
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(req.body.password, salt);
+            req.body.password = hashedPassword;
+        }
+        const user = await User.findByIdAndUpdate(req.params.id, req.body);
+        if (!user) return res.status(404).send('The user with the given ID was not found.');
+        res.send(user);
+    }catch(err){
+        res.status(400).send(err);
+    }
+    
 });
 
-model.exports = router;
+module.exports = router;
