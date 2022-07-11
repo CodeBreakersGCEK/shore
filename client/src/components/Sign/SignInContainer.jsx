032fe214f38b967d22127fa773224b7c3ca18eee
Harsh Keshri <harshkeshri1234567@gmail.com>
2022-07-11 20:25:44 +0530

    Added Sign up and Login

--- client/src/components/Sign/SignInContainer.jsx
+++ client/src/components/Sign/SignInContainer.jsx
@@ -1,25 +1,49 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
 import FieldLi from "./FieldLi";
 
 const SignInContainer = () => {
-  const [username, setUsername] = useState("");
+  const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
+  const Navigate = useNavigate();
+
+  const validateForm = () => {
+    return userName && password;
+  };
+
+  const submitHandler = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post("http://localhost:4000/login", {
+        userName,
+        password,
+      });
+      console.log(response);
+      // setUserName("");
+      // setPassword("");
+      alert("You are Logged In");
+      Navigate("/");
+    } catch (e) {
+      alert(e);
+    }
+  };
 
   return (
     <div className="flex items-center justify-center h-[90vh]">
       <div className="grow bg-foreground min-w-[25rem] w-[20vw] max-w-[35rem] mx-[1.5rem] py-[3rem] rounded-[0.8rem] flex justify-center items-center flex-col max500:min-w-[5rem]">
         <form
-          action="submit"
+          onSubmit={submitHandler}
           className="flex flex-col items-center justify-center"
           method="post"
         >
           <ul className="poppinsText px-[1.6rem] w-[100%]">
             <FieldLi
-              fieldName={"UserName"}
-              value={username}
+              fieldName={"userName"}
+              value={userName}
               onChange={(e) => {
-                setUsername(e.target.value);
+                setUserName(e.target.value);
               }}
               inputType={"text"}
             />
@@ -32,7 +56,10 @@ const SignInContainer = () => {
               inputType={"password"}
             />
           </ul>
-          <button className="bg-primary text-white mt-[0.5rem] px-[0.8rem] py-[0.3rem] rounded-[0.4rem] max500:text-[0.8rem] max500:px-[0.6rem] max500:py-[0.2rem] w-[5.8rem]">
+          <button
+            className="bg-primary text-white mt-[0.5rem] px-[0.8rem] py-[0.3rem] rounded-[0.4rem] max500:text-[0.8rem] max500:px-[0.6rem] max500:py-[0.2rem] w-[5.8rem]"
+            disabled={!validateForm()}
+          >
             Sign in
           </button>
         </form>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,26 @@
+import axios from "axios";
 import React, { useContext } from "react";
+import { useEffect } from "react";
 import NavContainer from "../components/Home/NavContainer";
 import PostContainer from "../components/Home/PostContainer";
 import RightContainer from "../components/Home/RightContainer";
 
 const Home = () => {
+  useEffect(() => {
+    const loadProfile = async () => {
+      try {
+        const response = await axios.get("http://localhost:4000/profile", {
+          id: "62cb30a51e1a35aec77033cf",
+        });
+        console.log(response);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+
+    loadProfile();
+  });
+
   return (
     <div className="flex bg-background  align-start justify-center pt-[1.5rem] w-[100]">
       <NavContainer />
